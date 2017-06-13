e4490dfe53d52fb9840f90b7ed56c255c76a22c4
chutipong <chutipong@fungjai.com>
2017-06-13 09:49:43 +0700

    fail(reboot): not working can't find module 'windows'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserPrebootModule } from 'preboot/browser';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'ng4-universal-template'}),
+    BrowserPrebootModule.replayEvents()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
+import { ServerPrebootModule } from 'preboot/server';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     ServerModule,
-    AppModule
+    AppModule,
+    ServerPrebootModule.recordEvents({ appRoot: 'app-root' })
   ],
   bootstrap: [AppComponent]
 })
